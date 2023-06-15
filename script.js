9c518bf367caf9348a16d69efa2c831015fdce77
ilyacnt <ilyagt2012@gmail.com>
2023-06-15 11:05:47 +0300

    Add description for tampermonkey

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('test')
+// ==UserScript==
+// @name         Gold Apple: Brands Warning | Fast Copy
+// @namespace    https://goldapple.ru/
+// @version      0.0.1
+// @description  This script warns about prohibited brands and allows for quickly copying product data
+// @author       Ilyacnt
+// @match        https://goldapple.ru/
+// @grant        none
+// ==/UserScript==
+
+console.log('Tampermonkey script test')
